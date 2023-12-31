36ea5ed06009f51ac124eaf1d65e646fb85bb296
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-12-31 15:35:36 -0300

    Ajsute

---
+++ cypress/fixtures/massaVoos.js
@@ -0,0 +1,18 @@
+module.exports.array = [
+{
+    'origem' : 'São Paolo',
+    'destino': 'Cairo'
+},
+{
+    'origem' : 'Portland',
+    'destino': 'Berlin'
+},
+{
+    'origem' : 'Mexico City',
+    'destino': 'Dublin'
+},
+{
+    'origem' : 'San Diego',
+    'destino': 'Rome'
+}
+]
