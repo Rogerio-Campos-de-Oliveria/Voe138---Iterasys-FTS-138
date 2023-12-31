11c10fd5d7632f5e327e4ef2b26490f82410de7c
Rogerio-Campos-de-Oliveira <monge.n@gmail.com>
2023-12-31 17:00:18 -0300

    Test Cypess - Terminado

--- cypress/e2e/ui/buscaVoos.cy.js
+++ cypress/e2e/ui/buscaVoos.cy.js
@@ -1,6 +1,10 @@
+
 describe('Buscar por voos', () => { // Inicio describe
 
     context('Não Logado', () => { // Inicio context
+
+        // importar a nossa massa de teste
+        const massaVoos = require('../../fixtures/massaVoos')
         
         beforeEach(() => { // Inicio do before
 
@@ -36,8 +40,119 @@ describe('Buscar por voos', () => { // Inicio describe
             // Validar o cabeçalho da página
             cy.get('.container h3')
             .should('have.text', 'Flights from São Paolo to Cairo: ')
-                
+
+            // Clicar no botão do primeiro vôo
+            cy.get('input.btn.btn-small')
+                .eq(0)
+                .click()
+
+            // Transição para página Purchase
+            // Validar o titulo da aba/guia
+            cy.title().should('eq', 'BlazeDemo Purchase')
+
+            // Escrever o primeiro nome
+            cy.get('#inputName').type('Jonas')
+
+            // Selecionar a bandeira do cartão
+            cy.get('#cardType')
+                .select('American Express')
+
+            // Marcar o checkbox Remember ME
+            cy.get('#rememberMe')
+                .check()
+
+            // Clicar no botão Purchase Flight
+            cy.get('.btn.btn-primary')
+                .click()
+
+            // Transição para a pagina final
+            // Validar o titulo da aba/guia
+            cy.title().should('eq', 'BlazeDemo Confirmation')
+
+            // Validar a mensagem de agradecimento
+            cy.get('.container h1')
+                .should('have.text', 'Thank you for your purchase today!')
+
+            // Validar o preço
+            cy.get('.table').contains('td', 'Amount')  // Encontra o <td> que contém 'Amount'
+                .siblings()                            // Obtém os irmãos deste <td>
+                .should('contain', '555 USD')          // Verifica se um desses irmãos contém '555 USD'
+
+
+
+
         });
+
+        // Este é o nosso teste data drive (usa uma massa de teste)
+        massaVoos.array.forEach(({origem, destino, voo, nome, bandeira}) => {
+            it(`Buscar voos entre ${origem} e ${destino}`, () => {
+                // Veja se o titulo da guia/aba é igual a 'BlazeDemo'
+                cy.title().should('eq', 'BlazeDemo')
+
+                // Seleciona o combo Origem pelo CssSelector
+                // Identificando que deve ser o primeiro (0)
+                // Selecionando a opção "São Paolo"
+                cy.get('select.form-inline')
+                    .eq(0)
+                    .select(origem)
+
+                cy.get('select.form-inline')
+                    .eq(1)
+                    .select(destino)
+
+                // Clicar no botão Find Flights / Procurar Voos
+                cy.get('.btn.btn-primary')
+                    .click()
+
+                // Transição para a página Reserve
+
+                // Validar o titulo da aba/guia
+                cy.title().should('eq', 'BlazeDemo - reserve')
+
+                // Validar o cabeçalho da página
+                cy.get('.container h3')
+                .should('have.text', `Flights from ${origem} to ${destino}: `)
+
+                 // Clicar no botão do botão relativo ao voo
+                cy.get('input.btn.btn-small')
+                    .eq(voo)
+                    .click()
+
+                // Transição para página Purchase
+                // Validar o titulo da aba/guia
+                cy.title().should('eq', 'BlazeDemo Purchase')
+
+                // Escrever o primeiro nome
+                cy.get('#inputName').type(nome)
+
+                // Selecionar a bandeira do cartão
+                cy.get('#cardType')
+                    .select(bandeira)
+
+                // Marcar o checkbox Remember ME
+                cy.get('#rememberMe')
+                    .check()
+
+                // Clicar no botão Purchase Flight
+                cy.get('.btn.btn-primary')
+                    .click()
+
+                // Transição para a pagina final
+                // Validar o titulo da aba/guia
+                cy.title().should('eq', 'BlazeDemo Confirmation')
+
+                // Validar a mensagem de agradecimento
+                cy.get('.container h1')
+                    .should('have.text', 'Thank you for your purchase today!')
+
+                // Validar o preço
+                cy.get('.table').contains('td', 'Amount')  // Encontra o <td> que contém 'Amount'
+                    .siblings()                            // Obtém os irmãos deste <td>
+                    .should('contain', '555 USD')          // Verifica se um desses irmãos contém '555 USD'
+                    
+            }) // final teste/ it
+
+        }); // final do for Each
 
     }); // final context
 
--- cypress/fixtures/massaVoos.js
+++ cypress/fixtures/massaVoos.js
@@ -1,18 +1,30 @@
 module.exports.array = [
 {
     'origem' : 'São Paolo',
-    'destino': 'Cairo'
+    'destino': 'Cairo',
+    'voo' : 0,
+    'nome' : 'Daniel',
+    'bandeira' : 'American Express'
 },
 {
     'origem' : 'Portland',
-    'destino': 'Berlin'
+    'destino': 'Berlin',
+    'voo' : 1,
+    'nome' : 'Cintia',
+    'bandeira' : 'Visa'
 },
 {
     'origem' : 'Mexico City',
-    'destino': 'Dublin'
+    'destino': 'Dublin',
+    'voo' : 2,
+    'nome' : 'Marcio',
+    'bandeira' : 'American Express'
 },
 {
     'origem' : 'San Diego',
-    'destino': 'Rome'
+    'destino': 'Rome',
+    'voo' : 3,
+    'nome' : 'Rosi',
+    'bandeira' : 'Visa'
 }
 ]
